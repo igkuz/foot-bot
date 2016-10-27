95485a60eb6fa6433366a91064ecb4fb79d55ec2
Igor Kuznetsov <ikuznetsov@setka.io>
2016-10-27 18:23:58 +0300

    Bot updates: Added support for /standings command Bot requests data from sport API and formats message with Markdown

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var TelegramBot = require('node-telegram-bot-api');
+var TelegramBot = require('node-telegram-bot-api'),
+    request = require('request');
 
 var token = process.env.FBOT_TOKEN;
 // Setup polling way
@@ -11,8 +12,40 @@ bot.onText(/\/echo (.+)/, function (msg, match) {
   bot.sendMessage(fromId, resp);
 });
 
-bot.onText(/\/markup (.+)/, function(msg, match) {
-  var resp = "1\tMan Utd\t\t\t30\n2\tMan City\t\t\t29\n"
-  bot.sendMessage(msg.from.id, resp);
+bot.onText(/\/standings/, function(msg, match) {
+  getStandingMessage(function(message) {
+    bot.sendMessage(msg.from.id, message, {parse_mode: "Markdown"});
+  });
 });
 
+function getStandingMessage(callback) {
+  var teams = [];
+  var message = "```text\n";
+      message += ["Pos", "Club\t", "Games", "Points"].join("\t\t\t") + "\n";
+
+  var standinsUrl = 'http://soccer.sportsopendata.net/v1/leagues/premier-league/seasons/16-17/standings';
+  request(standinsUrl, function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      teams = JSON.parse(body)['data']['standings'];
+
+      var maxLength = teams.reduce(function(a, b) { return a.team.length > b.team.length ? a : b; });
+
+      teams.forEach(function(element) {
+        element.team += " ".repeat((maxLength.team.length - element.team.length));
+        if (element.position < 10) {
+          element.position += " "
+        };
+
+        if (element.overall.points < 10) {
+          element.overall.points = " " + element.overall.points
+        }
+
+        message += [element.position, element.team, element.overall.matches_played, element.overall.points].join("\t");
+        message += "\n";
+      });
+      message += "```";
+
+      callback(message);
+    }
+  });
+}
